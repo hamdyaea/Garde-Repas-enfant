1f3b21356babab48f1ad633411a817bc14f8aa6a
hamdyaea <hamdy.aea@protonmail.com>
2020-01-19 13:21:01 +0100

    Code beauty

--- garde.py
+++ garde.py
@@ -7,37 +7,41 @@ import easygui_qt as easy
 import sqlite3
 
 
-
 def NewDb():
-    conn = sqlite3.connect('garde.db')
+    conn = sqlite3.connect("garde.db")
     c = conn.cursor()
-    c.execute('''CREATE TABLE repas
-                 (date text, repas text)''')
+    c.execute(
+        """CREATE TABLE repas
+                 (date text, repas text)"""
+    )
     conn.commit()
     conn.close()
 
+
 def main():
-    conn = sqlite3.connect('garde.db')
+    conn = sqlite3.connect("garde.db")
     c = conn.cursor()
     date = easy.get_date()
     easy.set_font_size(20)
-    Message = "Add to the database the "+str(date)
+    Message = "Add to the database the " + str(date)
     Title = "Confirm write to the database"
     easy.show_message(message=Message, title=Title)
     c.execute("INSERT INTO repas VALUES (?, ?);", (date, "Repas Shérine"))
     conn.commit()
     conn.close()
 
+
 def Read():
-    conn = sqlite3.connect('garde.db')
+    conn = sqlite3.connect("garde.db")
     c = conn.cursor()
     c.execute("SELECT * FROM repas")
     print(c.fetchall())
     conn.commit()
     conn.close()
 
+
 def Delete():
-    conn = sqlite3.connect('garde.db')
+    conn = sqlite3.connect("garde.db")
     c = conn.cursor()
     sql_delete_query = """DELETE from repas where date = '2020-01-28'"""
     c.execute(sql_delete_query)
@@ -47,6 +51,7 @@ def Delete():
     conn.commit()
     conn.close()
 
-#Delete()
-#main()
+
+# Delete()
+# main()
 Read()
